17bd0fb6f969d7989667f3af9dea14bb240e956b
Kierthana <kierthanars@gmail.com>
2025-11-27 17:54:59 +0530

    fix : updated zod to accept date as string

--- src/schemas/attendance.schema.ts
+++ src/schemas/attendance.schema.ts
@@ -3,12 +3,12 @@ import { z } from 'zod';
 
 export const createCheckIn = z.object({
   staff_id : z.number().int().positive(),
-  check_in: z.date(),
+  check_in: z.coerce.date(),
 });
 
 export const createCheckOut = z.object({
   attendance_id: z.number().int().positive(),
-  check_out: z.date(),
+  check_out: z.coerce.date(),
 });
 
 export type createCheckInInput= z.infer<typeof createCheckIn>;
